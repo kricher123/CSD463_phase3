a1c0cf764254929aae86cb07e54116c68b18d37c
notatimelord <150253431+notatimelord@users.noreply.github.com>
2025-06-05 14:44:18 +0300

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,14 @@
 public class main {
 
     public static void main(String[] args) throws Exception {
+        long startTime = System.nanoTime();
         searchEngine.topicRetrieval();
+        Results.print_results();
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        double seconds = duration / 1_000_000_000.0;
+        System.out.printf("Time taken: %.4f seconds%n", seconds);
+
     }
 }
